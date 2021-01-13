72d94f9460c0d3dc13fb837803c051f15815bcef
Matthew Berryman <matthew@acrossthecloud.net>
2021-01-14 09:10:46 +1100

    fix ordering and add java support

--- linux.sh
+++ linux.sh
@@ -6,6 +6,8 @@ curl https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-20
 sudo sh -c 'echo deb https://apt.repos.intel.com/mkl all main > /etc/apt/sources.list.d/intel-mkl.list'
 sudo apt-get update && sudo apt-get -y upgrade
 
+sudo apt-get -y install build-essential g++ gfortran bzip2 libbz2-dev xorg-dev liblzma-dev libreadline-dev libpcre2-dev libpcre++-dev libcurl4-openssl-dev libpango1.0-dev libjpeg9-dev libcairo2-dev openjdk-14-jdk openjdk-14-jre
+
 # remove old MKL version - safe to ignore any warnings. Also clean up packages.
 sudo apt-get -y remove 'intel-comp-l-all-var*' 'intel-comp-nomcu-vars*' 'intel-conda-index-tool*' 'intel-conda-intel-openmp*' 'intel-conda-mkl*' 'intel-conda-tbb*' 'intel-mkl*' 'intel-openmp*' 'intel-psxe-common*' 'intel-tbb-libs*'
 sudo apt-get -y clean
@@ -21,7 +23,6 @@ sudo ldconfig
 
 [ -f /opt/intel/mkl/bin/mklvars.sh ]  && source /opt/intel/mkl/bin/mklvars.sh intel64
 [ -f /opt/intel/mkl/bin/mklvars.sh ]  && export MKL="-Wl,--no-as-needed -lmkl_gf_lp64 -Wl,--start-group -lmkl_gnu_thread  -lmkl_core  -Wl,--end-group -fopenmp  -ldl -lpthread -lm"
-sudo apt-get -y install build-essential g++ gfortran bzip2 libbz2-dev xorg-dev liblzma-dev libreadline-dev libpcre2-dev libpcre++-dev libcurl4-openssl-dev libpango1.0-dev libjpeg9-dev libcairo2-dev
  
 curl -O https://cran.r-project.org/src/base/R-4/R-$RVERSION.tar.gz
 
