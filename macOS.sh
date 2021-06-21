6353a115c4fcc0f3cb62663a6e63f58a18962f07
Matthew Berryman <matthew@acrossthecloud.net>
2021-06-21 15:27:59 +1000

    build macOS with openmp and openblas using brew add Makevars for Linux and macOS

--- macOS.sh
+++ macOS.sh
@@ -1,36 +1,11 @@
 #!/usr/bin/env bash
 
 # requirements: 
-# * Intel MKL for macOS https://software.intel.com/en-us/mkl/choose-download
 # * Homebrew https://brew.sh/
-# * GNU Fortran: run `brew install gcc` sans quotes (GNU Fortran is included)
 # * X11 https://www.xquartz.org/ 
 # * XCode / command line developer tools for macOS (installing homebrew will install these)
 
-RVERSION=3.6.3
-export MKLROOT="/opt/intel/mkl"
-export MKL=" -L${MKLROOT}/lib ${MKLROOT}/lib/libmkl_blas95_ilp64.a ${MKLROOT}/lib/libmkl_lapack95_ilp64.a -lmkl_intel_ilp64 -lmkl_intel_thread -lmkl_core -liomp5 -lpthread -lm -lmkl_gf_ilp64"
-export arch=x86_64
-export r_arch=x86_64
-export CC="gcc -arch x86_64 -std=gnu99"
-export CXX="g++ -arch x86_64"
-export OBJC="clang"
-export F77="gfortran -arch x86_64"
-export FC="gfortran -arch x86_64"
-export CFLAGS='-g -O2'
-export CXXFLAGS='-g -O2'
-export FCFLAGS='-g -O2'
-export F77FLAGS='-g -O2'
-export PKG_CONFIG_PATH=/opt/X11/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/lib/pkgconfig
-
-curl -O https://cran.r-project.org/src/base/R-3/R-$RVERSION.tar.gz
-tar xf R-$RVERSION.tar.gz
-cd R-$RVERSION
-
-./configure --with-blas="${MKL}" --with-lapack --with-aqua --with-system-zlib --enable-memory-profiling --enable-R-framework --x-includes=/opt/X11/include --x-libraries=/opt/X11/lib --with-tcltk=/usr/local/lib
-make
-sudo make install
-
-# now fix up the bad arch detection:
-sed -i '' 's/R_ARCH\=\/\`arch\`/R_ARCH\=\/x86_64/g' /Library/Frameworks/R.framework/Versions/Current/Resources/bin/R
-
+brew install --cask mactex
+brew tap CBADS-SPI/homebrew-r-srf
+brew install icu4c libtiff openblas openjdk CBADS-SPI/r-srf/cairo-x11 CBADS-SPI/r-srf/tcl-tk-x11
+brew install CBADS-SPI/homebrew-r-srf/r --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-icu4c
