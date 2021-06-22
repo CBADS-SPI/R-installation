ad916fd76f5170775b0de67837607011ee457911
Matthew Berryman <matthew@acrossthecloud.net>
2021-06-22 12:16:45 +1000

    couple of tweaks

--- macOS.sh
+++ macOS.sh
@@ -7,7 +7,8 @@
 
 brew install --cask mactex
 brew tap CBADS-SPI/homebrew-r-srf
-brew install icu4c libtiff openblas openjdk CBADS-SPI/r-srf/cairo-x11 CBADS-SPI/r-srf/tcl-tk-x11
+brew install icu4c libtiff openblas libomp openjdk xz
+brew install CBADS-SPI/r-srf/cairo-x11 CBADS-SPI/r-srf/tcl-tk-x11
 brew install CBADS-SPI/homebrew-r-srf/r --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-icu4c
 
 sudo mkdir -p /Library/Frameworks/R.framework/Versions/Current/Resources/lib/
