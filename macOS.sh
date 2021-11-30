8e28c0b124fdbd7bc9f0d20e85a534792e8e41b3
Matthew Berryman <matthew@acrossthecloud.net>
2021-12-01 07:52:45 +1100

    use upstream

--- macOS.sh
+++ macOS.sh
@@ -6,10 +6,10 @@
 # * XCode / command line developer tools for macOS (installing homebrew will install these)
 
 brew install --cask mactex
-brew tap CBADS-SPI/homebrew-r-srf
+brew tap sethrfore/homebrew-r-srf
 brew install icu4c libtiff openblas libomp openjdk xz
-brew install CBADS-SPI/r-srf/cairo-x11 CBADS-SPI/r-srf/tcl-tk-x11
-brew install CBADS-SPI/homebrew-r-srf/r --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-icu4c
+brew install sethrfore/r-srf/cairo-x11 sethrfore/r-srf/tcl-tk-x11
+brew install sethrfore/r-srf/r --with-openblas --with-openjdk --with-tcl-tk-x11 --with-cairo-x11 --with-icu4c
 
 sudo mkdir -p /Library/Frameworks/R.framework/Versions/Current/Resources/lib/
 cd /Library/Frameworks/R.framework/Versions/Current/Resources/lib/
